d4df10f52714b27a4860b29f03dce400a3ad8734
akash-akya <akashh246@gmail.com>
2020-05-09 23:53:31 +0530

    Add test for checking memory leak

---
+++ test/exile/sync_process_test.exs
@@ -0,0 +1,26 @@
+defmodule Exile.SyncProcessTest do
+  use ExUnit.Case, async: false
+  alias Exile.Process
+
+  @bin Stream.repeatedly(fn -> "A" end) |> Enum.take(65535) |> IO.iodata_to_binary()
+
+  test "memory leak" do
+    :timer.sleep(1000)
+    before_exec = :erlang.memory(:total)
+
+    {:ok, s} = Process.start_link("cat", [])
+
+    Enum.each(1..500, fn _ ->
+      :ok = Process.write(s, @bin)
+      {:ok, _} = Process.read(s, 65535)
+    end)
+
+    :timer.sleep(1000)
+    after_exec = :erlang.memory(:total)
+
+    assert_in_delta before_exec, after_exec, 1024 * 1024
+
+    assert :ok == Process.close_stdin(s)
+    assert {:ok, {:exit, 0}} == Process.await_exit(s, 500)
+  end
+end
